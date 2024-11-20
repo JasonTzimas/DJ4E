73e03a24cbfd44777dfb61207085f4bd2d465312
JasonTzimas <jason.tzimas@gmail.com>
2024-11-20 14:29:43 +0200

    changes made

--- SimplestPythonServer/simple_server.py
+++ SimplestPythonServer/simple_server.py
@@ -3,7 +3,7 @@ from socket import *
 def createServer():
     serversocket = socket(AF_INET, SOCK_STREAM)
     try:
-        serversocket.bind('localhost', 9000)
+        serversocket.bind(('localhost', 9000))
         serversocket.listen(5)
         while(1):
             (clientsocket, address) = serversocket.accept()
